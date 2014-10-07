6d49c70a0452d0e2b1322106de1179975af18d9a
ericcgu <eric.changning.gu@gmail.com>
2014-10-06 22:01:20 -0400

    first commit

--- SwiftCoreLocation/ViewController.swift
+++ SwiftCoreLocation/ViewController.swift
@@ -1,25 +1,59 @@
 //
 //  ViewController.swift
 //  SwiftCoreLocation
-//
 //  Created by Eric Gu on 10/5/14.
 //  Copyright (c) 2014 Eric Gu. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager = CLLocationManager()
 
+    @IBOutlet weak var theMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.manager.requestWhenInUseAuthorization()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//MARK: location manager
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
+        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+        
+        println("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        let latitude:CLLocationDegrees = locValue.latitude
+        let longitude: CLLocationDegrees = locValue.longitude
+        
+        //change for Zoom Level
+        let latDelta: CLLocationDegrees = 0.5
+        let longDelta: CLLocationDegrees = 0.5
+        
+        //update the map
+        var theSpan: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMake(location, theSpan)
+        self.theMap.setRegion(theRegion, animated: true)
+        
+        //stop updating location for manual update
+        self.manager.stopUpdatingLocation()
+
     }
-
-
+    
+    @IBAction func updateMap(sender: AnyObject) {
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            manager.startUpdatingLocation()
+        }
+    }
+    
 }
 
